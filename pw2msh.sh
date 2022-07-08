34613f8a2be597db14a59d154685aed723803d76
stevenliuyi <stevenliuyi@gmail.com>
2022-07-08 03:41:01 -0400

    add option to generate msh2cdp.in

--- pw2msh.sh
+++ pw2msh.sh
@@ -8,12 +8,13 @@
 help() {
   echo "Convert Pointwise meshes to Fluent msh files."
   echo
-  info "Usage: pw2msh.sh [-opi] FILENAME"
+  info "Usage: pw2msh.sh [-o|p|i|c] FILENAME"
   echo
   echo "Options:"
   echo "  -o      Overwrite existing files"
   echo "  -p      Only convert .pw to .cas (using Pointwise)"
   echo "  -i      Only convert .cas to .msh (using ICEM)"
+  echo "  -c      Also generate msh2cdp.in when there are multiple parts"
   echo
 }
 
@@ -33,12 +34,14 @@ info () {
 default_overwrite=false # -o
 default_pw_only=false # -p
 default_icem_only=false # -i
+default_cdp=false # -c
 
-while getopts ":opi" opt; do
+while getopts ":opic" opt; do
   case $opt in
     o) overwrite=true ;;
     p) pw_only=true ;;
     i) icem_only=true ;;
+    c) cdp=true ;;
     \?) echo "Invalid option -$OPTARG" >&2
     exit 1 ;;
   esac
@@ -48,6 +51,7 @@ shift $((OPTIND-1))
 [ -z $overwrite ] && overwrite=$default_overwrite
 [ -z $pw_only ] && pw_only=$default_pw_only
 [ -z $icem_only ] && icem_only=$default_icem_only
+[ -z $cdp ] && cdp=$default_cdp
 
 if [ "$pw_only" = true ] && [ "$icem_only" = true ]; then
   echo "Options -p and -i cannot be used together!"
@@ -158,4 +162,30 @@ if [ "$pw_only" = false ]; then
 
   # clean temporary files
   rm -f *.fbc* *.atr *.prj *.uns *.tin *.blk
+fi
+
+#######################################
+# msh2cdp.in                          #
+#######################################
+if [ "$cdp" = true ]; then
+  cdp_file="msh2cdp.in"
+  count=$(find -maxdepth 1 -name "${name}_part*.msh" | wc -l)
+  if [ $count -gt 1 ]; then 
+    > $cdp_file
+    id=0
+    for filename in ${name}_part*.msh; do
+      [ -f "${filename}" ] || continue
+      id0=$(printf "%04d" $id)
+      echo "MSH.$id0=./${filename}" >> $cdp_file	    
+      ((id++))
+    done
+    echo "NPART=1000" >> $cdp_file
+    for ((id=0; id<$count; id++)); do
+      id0=$(printf "%04d" $id)
+      echo "RECONN$((id+1)).$id0=RECONNENT" >> $cdp_file	    
+    done
+    info "$cdp_file is generated."
+  else
+    info "There should be at least two parts to generate msh2cdp.in"
+  fi
 fi
